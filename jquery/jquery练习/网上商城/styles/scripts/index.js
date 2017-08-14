da1e808558a66e5a517ea51d120cc00166593416
wbh1234 <18249214675@163.com>
2017-08-14 21:55:58 +0800

    diyige

---
+++ jquery/jquery练习/网上商城/styles/scripts/index.js
@@ -0,0 +1,39 @@
+/**
+ * Created by ASUS on 2017/8/14.
+ */
+$(function(){
+    //搜索框
+    $("#inputSearch").on("focus", function(){
+        if(this.value == this.defaultValue){
+            this.value ="";
+        }
+    }).on("blur", function(){
+        if(this.value ==""){
+       this.value = this.defaultValue;
+        }
+
+    });
+    //导航菜单
+    $("#nav li").hover(function(){
+        $(this).children(".jnNav").show();
+    }, function(){
+        $(this).children(".jnNav").hide();
+    });
+    // $("#nav li a").hover(function(){
+    //     $(this).next().show();
+    // },function(){
+    //     $(this).next().hide();
+    // })
+    // $("#nav li a").on("mouseover", function(){
+    //     $(this).next().show();
+    // }).on("mouseout", function(){
+    //     $(this).next().hide();
+    // });
+    //hot
+    $("#jnCatalog.promoted").append("<span class='hot'> </span>");
+    // //轮播图
+    // //调整图片的层级关系：z-index
+        $("#jnImageroll div a").on("mouseover", function(){
+            $(this).addClass("chos").siblings().removeClass("chos");
+        });
+});
