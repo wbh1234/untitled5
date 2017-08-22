65557a3ef0bc85fd0c3996a39f434ab613bc4877
wbh1234 <18249214675@163.com>
2017-08-22 16:57:56 +0800

    diyige

--- requirejs/轮播图/js/carousel.js
+++ requirejs/轮播图/js/carousel.js
@@ -1,3 +1,24 @@
 /**
  * Created by ASUS on 2017/8/22.
  */
+define(["jquery"],function($){
+    function Carousel(settings){
+        this.$container = $('<div class="carousel-container"></div> ');
+        this.$tabs = $('<ul class="carousel-tabs" ></ul>  ');
+        this.$img = $('<div class="carousel-imgs"  ></div>');
+        this.$arrows = $('<div class="carousel-arrows"  ></div>');
+        this.$prev = $('<div class="carouser-prev"  >&lt;</div>');
+        this.$next = $('<div class="carouser-next"  >&gt;</div>');
+        this.defaultSettings = {
+            selector : document.body,
+            imgArr:[],
+            speed: 1000,
+            btnStyle:"square",
+            arrowPos:"bottom"
+        };
+        $.extend(this.defaultSetting, setting);
+
+
+
+    }
+});
--- requirejs/轮播图/js/index.js
+++ requirejs/轮播图/js/index.js
@@ -2,5 +2,25 @@
  * Created by ASUS on 2017/8/22.
  */
 require(["jquery", "carousel"], function($, Carousel){
+    var settings1 = {
+        selector:"#container1",
+        imgArr : ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"],
+        speed: 1000,
+        btnStyle:"square",//circle
+        arrowPos:"botton"//center
+    };
+    var carousell = new Carousel(settings1);
+    carousell.init();
+
+    var setting2 = {
+        selector : "container2",
+        imArr : ["img/2.jpg","img/3.jpg","img/4.jpg"],
+        speed: 500,
+        btnStyle:"circle",
+        arrowPos:"center"
+    };
+    var carouse12 = new Carousel(settings2);
+    carouse12.init();
+
 
 });
